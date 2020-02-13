a58eefe19d2cffd46531869759c7c150e84a854b
KMV <9117479@gmail.com>
2020-02-13 21:58:19 +0300

    Files created

---
+++ settings.py
@@ -0,0 +1,9 @@
+api_v = '5.21'
+token = ('2a0545aa68a19df8fb37eb969049ac8c06e0f80191a7608cb539ea6bb017b01296311ece43998aee0a577',
+		)
+my_id = '33058741'
+deep = 2
+broker = {'user': '', 'password':'', 'ip':'', 'vhost':'vk_friends'}
+backend = broker
+m_friends_dct = {'file': 'mutual_friends_dct', 'graph':'mutual_friends_g'}
+d_friends_dct = {'file': 'deep_friends_dct', 'graph': 'deep_friends_g'}
